abfca52f9a0d34125b2544fb1cead7d264f83285
Falrach94 <samantha.zehrer@gmail.com>
2024-07-06 00:24:39 +0200

    added method for finding regions for which indicator needs to be calculated

--- core/data/test/utils_test.py
+++ core/data/test/utils_test.py
@@ -3,10 +3,74 @@ import unittest
 
 import pandas as pd
 
-from data.utils import mark_chunks, split_time_chunks
+from core.data.technical_indicators.indicators import IndicatorDescription, IndicatorParameterDescription, IndicatorPrototype
+from data.utils import find_indicator_update_regions, mark_chunks, split_time_chunks
+
+
+class DummyIndicator(IndicatorPrototype):
+
+    def __init__(self, skip_f):
+        super().__init__()
+
+        self.params = [
+                IndicatorParameterDescription("a", 0, 10, 4, 'int'),
+                IndicatorParameterDescription("b", -1, 1, 0.2, 'float'),
+            ]
+        self._skip_f = skip_f
+
+    def apply_to_df(self, params: dict, df: pd.DataFrame):
+        df.at[params[self._skip_f]:, hash(self)] = 0
+
+    def get_descriptor(self) -> IndicatorDescription:
+        return IndicatorDescription(
+            "dummy",
+            params=self.params,
+            fct=self.apply_to_df,
+            norm_factor=3,
+            skip_field=self._skip_f
+        )
 
 
 class TestUtils(unittest.TestCase):
+    
+    def test_find_indicator_update_regions(self):
+        df = pd.concat([
+            pd.DataFrame({'time': pd.date_range(start='1/1/2024', periods=1, freq='min')}),
+            pd.DataFrame({'time': pd.date_range(start='1/10/2024', periods=10, freq='min')}),
+            pd.DataFrame({'time': pd.date_range(start='3/5/2024', periods=4, freq='min')})])
+        df = df.reset_index(drop=True)
+        indicator_hash = 'indicator'
+
+        # test with indicator that has not yet been added to the dataframe
+        update_list = find_indicator_update_regions(df, indicator_hash, 3)
+        self.assertEqual(update_list[0], (1, 10))
+        self.assertEqual(update_list[1], (11, 4))
+
+        # test with indicator that has already been added to the dataframe with all values null
+        df[indicator_hash] = None
+        update_list = find_indicator_update_regions(df, indicator_hash, 3)
+        self.assertEqual(len(update_list), 2)
+        self.assertEqual(update_list[0], (1, 10))
+        self.assertEqual(update_list[1], (11, 4))
+
+        # test with some values not null
+        df.at[3, indicator_hash] = 1
+        df.at[4, indicator_hash] = 1
+        df.at[5, indicator_hash] = 1
+        df.at[6, indicator_hash] = 1
+        df.at[13, indicator_hash] = 1
+        update_list = find_indicator_update_regions(df, indicator_hash, 2)
+        self.assertEqual(len(update_list), 2)
+        self.assertEqual(update_list[0], (5, 6))
+        self.assertEqual(update_list[1], (12, 3))
+
+        # test with all values not null in one time frame
+        df.at[14, indicator_hash] = 1
+        update_list = find_indicator_update_regions(df, indicator_hash, 2)
+        self.assertEqual(len(update_list), 1)
+        self.assertEqual(update_list[0], (5, 6))
+
+
     def test_split_time_chunks(self):
 
         # test dataframe with datetime entries
--- core/data/utils.py
+++ core/data/utils.py
@@ -67,6 +67,33 @@ def training_split(chunk_cnt: int,
     random.shuffle(split_list)
     return split_list
 
+### finds regions in the data where the indicator needs to be updated
+### returns a list of tuples with the start index and the length of the region
+def find_indicator_update_regions(data: pd.DataFrame, name: str, skip_cnt: int):
+    update_list = []
+    split_list = split_time_chunks(data)
+
+    fresh = name not in data.columns # indicator was not yet added to df
+
+    for (start_ix, frame_len) in split_list:
+        if frame_len > skip_cnt:
+            if fresh: 
+                ix = start_ix
+            else:
+                val_list = data.loc[start_ix + skip_cnt: start_ix + frame_len,
+                                    name]
+                val_list = val_list[val_list.isnull()]
+
+                # check if time frame is already complete
+                if len(val_list) == 0:
+                    continue
+
+                ix = val_list.index.min() - skip_cnt
+            update_list.append((ix, start_ix + frame_len - ix))
+
+    return update_list
+    
+
 def apply_indicator(data: pd.DataFrame, required_indicator: dict):
     def convert(x, type): 
         if type == 'int':

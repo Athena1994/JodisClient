66e6148897a4bd315b2788f0188656d855f1e801
Falrach94 <samantha.zehrer@gmail.com>
2024-07-10 22:50:29 +0200

    added normalization information to data_provider

--- core/data/data_provider.py
+++ core/data/data_provider.py
@@ -1,6 +1,5 @@
 
-
-import numpy as np
+from typing import Tuple
 import pandas as pd
 import torch
 
@@ -16,9 +15,10 @@ class DataProvider:
         return pd.read_csv(file)
 
     @staticmethod
-    def _normalize_col(df: pd.DataFrame, col: str, strategy: str) -> pd.DataFrame:
+    def _normalize_col(df: pd.DataFrame, col: str, strategy: str) \
+        :
         if strategy == 'none':
-            return df
+            return df, {'type': 'none'}
         elif strategy == 'warn':
             mean = df[col].mean()
             std = df[col].std()
@@ -31,50 +31,58 @@ class DataProvider:
                 print(f"Warning: std of column {col} is {std}")
             if abs(min_val) > 1.5 or abs(max_val) > 1.5:
                 print(f"Warning: min/max of column {col} is {min_val}/{max_val}")
-            return df
+            return df, {'type': 'warn', 'mean': mean, 'std': std, 'min': min_val, 'max': max_val}
 
         elif strategy == 'minmax':
             min_val = df[col].min()
             max_val = df[col].max()
             df[col] = (df[col] - min_val) / (max_val - min_val)
-            return df
+            return df, {'type': 'minmax', 'min': min_val, 'max': max_val}
         elif strategy == 'zscore':
             mean = df[col].mean()
             std = df[col].std()
             df[col] = (df[col] - mean) / std
-            return df
+            return df, {'type': 'zscore', 'mean': mean, 'std': std}
         else:
             raise ValueError(f"Normalization strategy {strategy} not supported!")
 
     def __init__(self, 
                  df: pd.DataFrame, 
                  required_columns: list[str],
-                 normalization_strategy: str):
+                 normalization_strategy: str, 
+                 window_size: int = 1):
 
-        self._df = df
+        self._df_original = df
+        self._df_normalized = df.copy()
 
-        missing_cols = [col for col in required_columns if col not in self._df]
+        # check if all required columns are present in the data
+        missing_cols = [col for col in required_columns if col not in self._df_normalized]
         if len(missing_cols) > 0:
             raise ValueError(f"Columns {missing_cols} not found in data.")
         self._required_columns = required_columns
 
+
+
+        # normalize the required columns
+        self._normalization_meta = {}
         for col in self._required_columns:
-            self._df = DataProvider._normalize_col(self._df, 
+            self._df_normalized, meta = DataProvider._normalize_col(self._df_normalized, 
                                                    col, 
                                                    normalization_strategy)
+            self._normalization_meta[col] = meta
 
-        chunk_ids = self._df[self._df['chunk'] != -1]['chunk'].unique()
+        chunk_ids = self._df_normalized[self._df_normalized['chunk'] != -1]['chunk'].unique()
 
         self._chunk_ixs \
-            = [(self._df[self._df['chunk'] == chunk_id].index[0],
-                len(self._df[self._df['chunk'] == chunk_id]))
+            = [(self._df_normalized[self._df_normalized['chunk'] == chunk_id].index[0],
+                len(self._df_normalized[self._df_normalized['chunk'] == chunk_id]))
               for chunk_id in chunk_ids]
         
 
         # lists of chunk ids with chunk type 0 (tr) 1(val) 2(test)
         self._chunk_ids = {'tr': [], 'val': [], 'test': []}
         for id in chunk_ids:
-            type = self._df[self._df['chunk'] == id]['chunk_type'].iloc[0]
+            type = self._df_normalized[self._df_normalized['chunk'] == id]['chunk_type'].iloc[0]
             if type == ChunkType.TRAINING:
                 self._chunk_ids['tr'].append(id)
             elif type == ChunkType.VALIDATION:
@@ -86,7 +94,8 @@ class DataProvider:
     def get_iterator(self, chunk_type: str) -> 'ChunkIt':
         if chunk_type not in ['tr', 'val', 'test']:
             raise ValueError(f"Chunk type {chunk_type} not supported.")
-        return DataProvider.ChunkIt(self._df,
+        return DataProvider.ChunkIt(self._df_normalized,
+                                    self._df_original,
                                     self._required_columns,
                                     self._chunk_ids[chunk_type],
                                     self._chunk_ixs)
@@ -96,14 +105,19 @@ class DataProvider:
             raise ValueError(f"Chunk type {chunk_type} not supported.")
         return len(self._chunk_ids[chunk_type])
 
+    def get_normalization_info(self) -> dict:
+        return self._normalization_meta
 
     class ChunkIt:
         def __init__(self, 
-                     df: pd.DataFrame, 
+                     df_normalized: pd.DataFrame,
+                     df_orginal: pd.DataFrame, 
                      required_columns: list[str],
                      ids: list[int],
-                     chunk_ixs: list[tuple[int, int]]):
-            self._df = df
+                     chunk_ixs: list[tuple[int, int]],
+                     window_size: int):
+            self._df_normalized = df_normalized
+            self._df_original = df_orginal
             self._chunk_ixs = chunk_ixs
             self._chunk_ids = ids
             self._required_columns = required_columns
@@ -121,6 +135,7 @@ class DataProvider:
 
             self._ix += 1
 
-            df = self._df.iloc[chunk_ix[0]:chunk_ix[0]+chunk_ix[1]][self._required_columns]
+            df_n = self._df_normalized.iloc[chunk_ix[0]:chunk_ix[0]+chunk_ix[1]][self._required_columns]
+            df_o = self._df_original.iloc[chunk_ix[0]:chunk_ix[0]+chunk_ix[1]]
 
             return torch.tensor(df.values, dtype=torch.float32)

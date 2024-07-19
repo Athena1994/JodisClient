5df9cd4dd0f4b448819ca0211a66027cf2747f43
Falrach94 <samantha.zehrer@gmail.com>
2024-07-19 11:51:52 +0200

    added tests to state manager and minor fixes

--- core/simulation/state_manager.py
+++ core/simulation/state_manager.py
@@ -15,21 +15,25 @@ class StateManager:
         self._samples = None
         
     def reset_state(self, context: dict) -> None:
-        self._context = context
+        self._context = context.copy()
         self._samples = None
 
     def update_context(self, 
                        context: dict) -> None:
                      
-        self._context = context
+        self._context = context.copy()
 
     def update_samples(self,
                        samples: Dict[str, Sample]) -> None:
-        self._samples = samples
+        self._samples = samples.copy()
 
     def get_context(self) -> dict:
-        return self._context.copy()
+        if self._context is None:
+            return None
+        return self._context.copy() 
     def get_samples(self) -> Dict[str, Sample]:
+        if self._samples is None:
+            return None
         return self._samples.copy()
     def get_state(self) -> State:
         return State(self.get_samples(), self.get_context())
@@ -59,11 +63,14 @@ class StateProvider(ContinuousProvider):
     
     def __next__(self) -> Sample:
         context = self._state_manager.get_context()
+        if context is None:
+            return None
+
         context_df = DataFrame(context, index=[0])[self._fields]
         n_df = self._normalizer.normalize_df(context_df,
                                              self._normalizer_conf)
 
-        return Sample(Tensor(n_df.values), context)
+        return Sample(Tensor(n_df.values).flatten(), context)
 
     def update_sample(self, sample: Sample) -> Sample:
         return next(self)

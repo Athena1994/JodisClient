4002bb47b8a10e3e58c426794a0caa09a1b1adcb
Falrach94 <samantha.zehrer@gmail.com>
2024-07-22 20:37:46 +0200

    expanded  dynamic nn

--- core/nn/dynamic_nn.py
+++ core/nn/dynamic_nn.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from core.data.technical_indicators.collection import IndicatorCollection
@@ -5,24 +6,7 @@ from core.data.technical_indicators.collection import IndicatorCollection
 
 class DynamicNN(nn.Module):
     @staticmethod
-    def _determine_input_size(data_desc: dict):
-        size = 0
-
-        for data in data_desc:
-            if data['ohcl']:
-                size += 4
-
-            if data['volume']:
-                size += 1
-
-            for indicator in data['indicators']:
-                ind_desc = IndicatorCollection.get(indicator['name'])
-                size += ind_desc.get_value_cnt()
-
-        return size
-
-    @staticmethod
-    def _create_layers(input_size: int, structure: list):
+    def _create_sequential(input_size: int, structure: list):
         layers = []
 
         layer_input = input_size
@@ -38,26 +22,76 @@ class DynamicNN(nn.Module):
                 layer_input = layer_desc['size']
             else:
                 raise ValueError(f"Unknown layer type: {layer_desc['type']}")
-        return nn.Sequential(*layers)
+        return nn.Sequential(*layers), layer_input
 
-    def __init__(self, config: dict):
+    def __init__(self, nn_config: dict, input_config: dict):
         super(DynamicNN, self).__init__()
 
-        structure = config['architecture']
+        def determine_input_size(input_desc: dict):
+            size = len(input_desc['params'].get('include', []))
 
-        input_size = self._determine_input_size(config['data'])
-        hidden_size = structure['LSTM']['hidden_size']
+            for indicator in input_desc['params'].get('indicators', []):
+                ind_desc = IndicatorCollection.get(indicator['name'])
+                size += ind_desc.get_value_cnt()
 
-        self.LSTM = nn.LSTM(input_size=input_size,
-                            hidden_size=hidden_size,
-                            num_layers=structure['LSTM']['num_layers'],
-                            batch_first=True)
+            return size
 
-        self.classifier = self._create_layers(hidden_size,
-                                              structure['classifier'])
+        def make_unit(type: str, input_size: int, params: dict):
+            if type.upper() == 'LSTM'.upper():
+                return nn.LSTM(input_size=input_size,
+                               hidden_size=params['hidden_size'],
+                               num_layers=params['num_layers'],
+                               batch_first=True), params['hidden_size']
+            elif type.upper() == 'Sequence'.upper():
+                return self._create_sequential(input_size, params)
+            else:
+                raise ValueError(f"Unknown unit type: {type}")
 
-    def forward(self, x):
-        x, _ = self.LSTM(x)
-        x = x[:, -1, :]
-        x = self.classifier(x)
-        return x
+        output_size = {d['key']: determine_input_size(d)
+                       for d in input_config}
+
+        self._units = []
+
+        for unit_conf in nn_config['units']:
+            inputs = unit_conf['input']
+            if not isinstance(inputs, list):
+                inputs = [inputs]
+
+            missing = [i for i in inputs if i not in output_size]
+            if len(missing) != 0:
+                raise ValueError(f"Missing input data: {missing}")
+
+            name = unit_conf['name']
+
+            unit = {}
+            unit['concat'] = len(inputs) > 1
+            unit['name'] = name
+            input_size = sum([output_size[i] for i in inputs])
+            unit['module'], output_size[name] = make_unit(unit_conf['type'],
+                                                          input_size,
+                                                          unit_conf['params'])
+            unit['input'] = inputs
+            self._units.append(unit)
+
+        if 'output' not in nn_config:
+            raise ValueError("Output key missing in nn configuration.")
+
+        self._output_key = nn_config['output']
+        if self._output_key not in output_size:
+            raise ValueError(f"Output key {self._output_key} not found")
+
+    def forward(self, x_dict):
+        for unit in self._units:
+            if unit['concat']:
+                inputs = [x_dict[i] for i in unit['input']]
+                x = torch.cat(inputs, dim=1)
+            else:
+                x = x_dict[unit['input'][0]]
+
+            x = unit['module'](x)
+            if isinstance(unit['module'], nn.LSTM):
+                x = x[0][:, 0, :]
+
+            x_dict[unit['name']] = x
+
+        return x_dict[self._output_key]
--- core/nn/test/test_dynamic_nn.py
+++ core/nn/test/test_dynamic_nn.py
@@ -9,22 +9,26 @@ from core.nn.dynamic_nn import DynamicNN
 
 class TestDynamicNN(unittest.TestCase):
 
-    def test_instantiation(self):
+    CONF = {
+        "general": {
+            "name": "AgentAlpha",
+            "unnormalized_data": "zscore",
+            "input_window": 128
+        },
 
-        dummy_conf = {
-            "general": {
-                "name": "AgentAlpha",
-                "unnormalized_data": "zscore",
-                "input_window": 128
-            },
-
-            "data": [
-                {
+        "input": [
+            {
+                "key": "ts",
+                "type": "asset",
+                "params": {
                     "asset": {
                         "name": "BTCEUR",
                         "source": "default",
                         "interval": "ONE_MINUTE"
                     },
+
+                    "include": ["volume", "open", "high", "close", "low"],
+
                     "indicators": [
                         {
                             "name": "AwesomeOscillator",
@@ -34,50 +38,145 @@ class TestDynamicNN(unittest.TestCase):
                             }
                         }
                     ],
-                    "volume": True,
-                    "ohcl": True
+
+                    "normalizer": {
+                        "df_key": "default",
+                        "default_strategy": "zscore",
+                        "groups": [["open", "high", "close", "low"]]
+                    }
+                }
+            },
+            {
+                "key": "meta",
+                "type": "state",
+                "params": {
+                    "include": [
+                        "balance",
+                        "position_open"
+                    ],
+                    "normalizer": {
+                        "default_strategy": "none",
+
+                        "extra":
+                        {
+                            "balance": {
+                                "strategy": "formula",
+                                "params": {
+                                    "expression": "np.log(x)/np.log(1.10)"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+
+        "nn": {
+            "units": [
+                {
+                    "name": "LSTM",
+                    "input": "ts",
+                    "type": "LSTM",
+                    "params": {
+                        "num_layers": 1,
+                        "hidden_size": 128
+                    }
+                },
+                {
+                    "name": "fc",
+                    "input": ["meta", "LSTM"],
+                    "type": "Sequence",
+                    "params": [
+                        {"type": "ReLU"},
+                        {"type": "Dropout", "p": 0.5},
+                        {"type": "Linear", "size": 256}
+                    ]
+                },
+                {
+                    "name": "classifier",
+                    "input": "fc",
+                    "type": "Sequence",
+                    "params": [
+                        {"type": "ReLU"},
+                        {"type": "Dropout", "p": 0.5},
+                        {"type": "Linear", "size": 128},
+                        {"type": "ReLU"},
+                        {"type": "Dropout", "p": 0.5},
+                        {"type": "Linear", "size": 3}
+                    ]
                 }
             ],
-
-            "architecture": {
-                "LSTM": {
-                    "num_layers": 1,
-                    "hidden_size": 256,
-                },
-
-                "classifier": [
-                    {"type": "ReLU"},
-                    {"type": "Dropout", "p": 0.5},
-                    {"type": "Linear", "size": 128},
-                    {"type": "ReLU"},
-                    {"type": "Dropout", "p": 0.6},
-                    {"type": "Linear", "size": 3}
-                ]
-            }
+            "output": "classifier"
         }
+    }
 
-        dnn = DynamicNN(dummy_conf)
+    def test_init(self):
 
-        self.assertEqual(dnn.LSTM.input_size, 4 + 1 + 1)
+        dnn = DynamicNN(self.CONF['nn'], self.CONF['input'])
 
-        self.assertEqual(dnn.LSTM.hidden_size, 256)
-        self.assertEqual(dnn.LSTM.num_layers, 1)
+        self.assertEqual(len(dnn._units), 3)
 
-        self.assertEqual(len(dnn.classifier), 6)
+        # --- lstm
 
-        self.assertTrue(isinstance(dnn.classifier[0], torch.nn.ReLU))
-        self.assertTrue(isinstance(dnn.classifier[1], torch.nn.Dropout))
-        self.assertEqual(dnn.classifier[1].p, 0.5)
-        self.assertTrue(isinstance(dnn.classifier[2], torch.nn.Linear))
-        self.assertEqual(dnn.classifier[2].in_features, 256)
-        self.assertEqual(dnn.classifier[2].out_features, 128)
-        self.assertTrue(isinstance(dnn.classifier[3], torch.nn.ReLU))
-        self.assertTrue(isinstance(dnn.classifier[4], torch.nn.Dropout))
-        self.assertEqual(dnn.classifier[4].p, 0.6)
-        self.assertTrue(isinstance(dnn.classifier[5], torch.nn.Linear))
-        self.assertEqual(dnn.classifier[5].in_features, 128)
-        self.assertEqual(dnn.classifier[5].out_features, 3)
+        self.assertEqual(dnn._units[0]['name'], 'LSTM')
+        self.assertEqual(dnn._units[0]['concat'], False)
+        self.assertTrue(isinstance(dnn._units[0]['module'],
+                                   torch.nn.LSTM))
+        self.assertEqual(dnn._units[0]['module'].input_size, 6)
+        self.assertEqual(dnn._units[0]['module'].hidden_size, 128)
 
-        test_data = torch.rand(16, 64, 6)
-        output = dnn(test_data)
-        self.assertEqual(output.size(), (16, 3))
+        # --- fc
+
+        self.assertEqual(dnn._units[1]['name'], 'fc')
+        self.assertEqual(dnn._units[1]['concat'], True)
+        self.assertTrue(isinstance(dnn._units[1]['module'],
+                                   torch.nn.Sequential))
+        self.assertTrue(isinstance(dnn._units[1]['module'][0],
+                                   torch.nn.ReLU))
+        self.assertTrue(isinstance(dnn._units[1]['module'][1],
+                                   torch.nn.Dropout))
+        self.assertEqual(dnn._units[1]['module'][1].p, 0.5)
+        self.assertTrue(isinstance(dnn._units[1]['module'][2], torch.nn.Linear))
+        self.assertEqual(dnn._units[1]['module'][2].in_features, 130)
+        self.assertEqual(dnn._units[1]['module'][2].out_features, 256)
+
+        # --- classifier
+
+        self.assertEqual(dnn._units[2]['name'], 'classifier')
+        self.assertEqual(dnn._units[2]['concat'], False)
+        self.assertTrue(isinstance(dnn._units[2]['module'],
+                                   torch.nn.Sequential))
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][0], torch.nn.ReLU))
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][1],
+                                   torch.nn.Dropout))
+        self.assertEqual(dnn._units[2]['module'][1].p, 0.5)
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][2], torch.nn.Linear))
+        self.assertEqual(dnn._units[2]['module'][2].in_features, 256)
+        self.assertEqual(dnn._units[2]['module'][2].out_features, 128)
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][3], torch.nn.ReLU))
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][4],
+                                   torch.nn.Dropout))
+        self.assertEqual(dnn._units[2]['module'][4].p, 0.5)
+
+        self.assertTrue(isinstance(dnn._units[2]['module'][5], torch.nn.Linear))
+        self.assertEqual(dnn._units[2]['module'][5].in_features, 128)
+        self.assertEqual(dnn._units[2]['module'][5].out_features, 3)
+
+    def test_forward(self):
+
+        dnn = DynamicNN(self.CONF['nn'], self.CONF['input'])
+
+        # --- input
+
+        ts = torch.rand(5, 128, 6)
+        meta = torch.rand(5, 2)
+
+        # --- forward
+
+        out = dnn({'ts': ts, 'meta': meta})
+        self.assertEqual(out.size(), (5, 3))
